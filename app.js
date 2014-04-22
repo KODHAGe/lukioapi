3e44ca5286ff1eb54311eb65ef1d70d474dab978
Wolf Wikgren <wwikgren@wakkerzeel.local>
2014-04-22 09:57:26 +0300

    Initial version
    0.0.1

---
+++ app.js
@@ -0,0 +1,95 @@
+// Yksinkertainen RESTify-rajapinta lukiodatalle
+// v 0.0.1
+// &copy; WRW
+
+// Ladataan vaatimukset asennettaessa:
+// restify & mongojs
+var restify = require('restify');
+var mongojs = require('mongojs');
+
+// API:n julkinen osoite ja kuunteluportti
+var ip_addr = '127.0.0.1';
+var port = '6565';
+
+// Luodaan restify-palvelin
+var server = restify.createServer({
+	name : "testapp"
+});
+
+// Käynnistetään restify-palvelin
+server.listen(port, ip_addr, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
+
+// Ladataan restify-lisäosia
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+// MongoDB:n asetukset
+var connection_string = '127.0.0.1:27017/test';
+var db = mongojs(connection_string, ['test']);
+var schools = db.collection('example');
+
+// Rajapinnan hakuarvot
+var PATH = '/lukio'
+server.get({path : PATH , version : '0.0.1'} , findAllSchools);
+server.get({path : PATH +'/:schoolId' , version : '0.0.1'} , findSchool);
+server.post({path : PATH , version: '0.0.1'} ,postNewSchool);
+
+// Hakufunktiot
+
+// Haetaan koulujen top30
+function findAllSchools(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    schools.find().sort({rank: -1}).limit(30 , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.charSet('utf-8');
+            res.send(200 , success);
+            return next();
+        }else{
+            return next(err);
+        }
+ 
+    });
+ 
+}
+
+// Koulu ID:n perusteella
+function findSchool(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    schools.findOne({_id:mongojs.ObjectId(req.params.schoolId)} , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.charSet('utf-8');
+            res.send(200 , success);
+            return next();
+        }
+        return next(err);
+    })
+}
+
+// Uusi koulu (WIP)
+function postNewSchool(req , res , next){
+    var school = {};
+    school.title = req.params.title;
+    school.description = req.params.description;
+    school.location = req.params.location;
+    school.postedOn = new Date();
+ 
+    res.setHeader('Access-Control-Allow-Origin','*');
+ 
+    schools.save(school , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(201 , school);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
+}
---
+++ js/mapScript.js
@@ -0,0 +1,105 @@
+// Piirretään yksi (1) kpl lukiovertailukarttaa.
+// Copyright 2013 STT-Lehtikuva
+
+// luetaan kartan muuttjat urlista (pituuspiiri, leveyspiiri, zoomausaste)
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+
+var alZoom = getUrlVars()["zoom"];
+var alLat = getUrlVars()["lat"];
+var alLong = getUrlVars()["lon"];
+
+// jos muuttujia ei ole määritelty urlissa, käytetään oletusarvoja
+
+if (alZoom==undefined || alZoom.length <= 0)
+{
+var alZoom = 6;
+}
+if (alLat==undefined || alLat.length <= 0)
+{
+var alLat = 62.241222;
+}
+if (alLong==undefined || alLong.length <= 0)
+{
+var alLong = 25.742411;
+}
+
+
+// Luodaan kartta #map-diviin ja asetetaan alkulat -long ja -zoom
+var map = L.map('map').setView([
+alLat,alLong], alZoom);
+
+// Lisätään MapQuest karttakerros; footeriin attriuutio käyttöehtojen mukaisesti
+          L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy;  <a href="www.openstreetmap.org/copyright">OpenStreetMap</a> — Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+          }).addTo(map);
+
+// Luodaan AwesomeMarkers-pluginilla ikoneja aseteltavaksi kartalle (tähän jotain hienompaa?)
+/*var redMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'red'
+})
+
+var orangeMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'orange'
+})
+
+var greenMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'green'
+})
+
+var darkgreenMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'darkgreen'
+})
+
+var cadetblueMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'cadetblue'
+})
+
+var blueMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'blue'
+})
+
+var darkblueMarker = L.AwesomeMarkers.icon({
+  icon: 'empty', 
+  color: 'darkblue'
+})*/
+
+// Poimitaan erillisessä .js:ssä tallennettu lukiodata ja piirretään markerit kartalle sen mukaisesti
+// Parhaat lukiot (muutos > 0.5) saavat kirkkaan vihreän, puolenvälin ylittävät (muutos > 0) tumman vihreän, puolenvälin alittavat (muutos < 0) oranssin ja huonoiten pärjänneet (muutos < -0.5) punaisen merkin.
+/*for (var i = 0; i < mapdata.length; i++) {
+	var markerType;
+	var val = parseFloat((mapdata[i][3]).replace(/,/, '.'));;
+	
+	if (val >= 0.2) {
+	markerType = greenMarker;
+	} else if (val >= -0.2 && val <= 0.2) {
+	markerType = orangeMarker;
+	} else if (val < -0.2) {
+	markerType = redMarker;
+	}
+	
+	
+	if (val >= 0.2) {
+	markerType = blueMarker;
+	} else if (val >= -0.2 && val <= 0.2) {
+	markerType = darkblueMarker;
+	} else if (val < -0.2) {
+	markerType = cadetblueMarker;
+	}
+	
+	
+	L.marker([mapdata[i][1],mapdata[i][2]], {icon:markerType}).addTo(map).bindPopup(mapdata[i][5]+" <a href='index.html?zoom=12&lat="+mapdata[i][1]+"&lon="+mapdata[i][2]+"'>Linkki tähän kouluun.</a><br/><br/><iframe src='http://www.facebook.com/plugins/like.php?href=http://09695811.net/lukiovertailu/index.html?zoom=12&lat="+mapdata[i][1]+"&lon="+mapdata[i][2]+"&amp;send=false&amp;layout=button_count&amp;width=150&amp;show_faces=false&amp;font=arial&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=167389803437269' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:150px; height:21px;' allowTransparency='true'></iframe><br/><iframe allowtransparency='true' frameborder='0' scrolling='no' src='https://platform.twitter.com/widgets/tweet_button.html?url=http://09695811.net/lukiovertailu/index.html?zoom=12&lat="+mapdata[i][1]+"&lon="+mapdata[i][2]+"' style='width:130px; height:20px;'></iframe>");
+	}*/
+    
